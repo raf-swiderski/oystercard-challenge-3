f1810dd51ee3c86258da713160d4047748037d97
Raf Swiderski <rafswid@icloud.com>
2021-02-11 15:47:21 +0000

    Chapter12

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,25 +1,33 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  before(:each) do
+    @oystercard = Oystercard.new
+  end
+
   it 'has a balance of 0 by default' do
-    expect(subject.balance).to eq(0)
+    expect(@oystercard.balance).to eq(0)
   end
   it 'has a limit' do
-    expect(subject.limit).to eq(90)
+    expect(@oystercard.limit).to eq(90)
   end
   it 'has an `in_use` status of false' do
-    expect(subject.in_use).to be false
+    expect(@oystercard.in_use).to be false
   end
 
   describe '#top_up' do
+
+    before(:each) do
+      @oystercard = Oystercard.new
+    end
+
     it 'should take a top_up value and add it to the card balance' do
-      subject.top_up(10)
-      expect(subject.balance).to eq (10)
+      @oystercard.top_up(10)
+      expect(@oystercard.balance).to eq (10)
     end
     it 'should raise an error if top_up returns more than 90' do
-      oyster = Oystercard.new
-      oyster.top_up(Oystercard::LIMIT)
-      expect { subject.top_up(91) }.to raise_error("top up limit of #{Oystercard::LIMIT} exceeded")
+      expect { @oystercard.top_up(91) }.to raise_error("top up limit of #{Oystercard::LIMIT} exceeded")
     end
   end
 
@@ -34,26 +42,23 @@ describe Oystercard do
       @oystercard.tap_in
       expect(@oystercard.in_journey?).to eq true
     end
-  #  it 'should return true if the oystercard is in use' do
-  #    expect(subject.in_journey?).to eq true
-  #  end
+
   end
 
   describe '#tap_in' do
 
-  #  before(:all) do
-  #    @oystercard = Oystercard.new
-  #  end
+    before(:each) do
+      @oystercard = Oystercard.new
+    end
 
     it 'should change in_journey? to true' do
-      subject.top_up(20)
-      expect{subject.tap_in}.to change{subject.in_journey?}.to true
+      @oystercard.top_up(20)
+      expect{@oystercard.tap_in}.to change{@oystercard.in_journey?}.to true
     end
     it 'should fail if balance is less than 1' do
       expect{ subject.tap_in }.to raise_error "Mininum balance of #{Oystercard::Min_Balance} required to travel"
     end
     it 'stores given station in @entry_station' do
-      @oystercard = Oystercard.new
       @oystercard.top_up(20)
       @oystercard.tap_in('Victoria')
       expect(@oystercard.entry_station).to eq 'Victoria'
@@ -64,24 +69,29 @@ describe Oystercard do
 
   describe '#tap_out' do
 
-    before(:all) do
+    before(:each) do
       @oystercard = Oystercard.new
+      @oystercard.top_up(10)
+      @oystercard.tap_in('Victoria')
     end
 
     it 'should change in_journey? to false' do
-      @oystercard.top_up(20)
-      @oystercard.tap_in
       expect { @oystercard.tap_out }.to change { @oystercard.in_journey? }.to false
     end
 
     it 'should deduct the balance by minimum fare' do
-      oystercard = Oystercard.new
-      oystercard.top_up(10)
-      oystercard.tap_in
-      oystercard.tap_out
-      expect(oystercard.balance).to eq (9)
+      @oystercard.tap_out
+      expect(@oystercard.balance).to eq (9)
+    end
+
+    it 'should store the stations in an array' do
+      @oystercard.tap_out('Woking')
+      expect(@oystercard.journeys.length).to eq 1
+    end
+
+    it 'should check that the card has an empty list of journeys by default' do
+      expect(@oystercard.journeys).to eq []
     end
 
   end
-
 end
